b715a645ff32b8769ab4fe69bab6172034d8a553
adiandrea <dekz.itfz@gmail.com>
2025-01-25 22:24:15 +0800

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Adi Andrea on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
